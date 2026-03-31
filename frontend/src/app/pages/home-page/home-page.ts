ef00a73d6755d22fee6e3dc7c1c97fc0b2f27cb9
Neil Kiuchi <neilkiuc@gmail.com>
2026-04-01 01:06:56 +0200

    add simple home page

--- frontend/src/app/pages/home-page/home-page.ts
+++ frontend/src/app/pages/home-page/home-page.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, RouterLink],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
