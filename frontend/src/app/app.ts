250d5dbd7fea16f8451d353476673542aaeb2578
Neil Kiuchi <neilkiuc@gmail.com>
2026-04-14 13:00:20 +0200

    fix: ajout main.ts manquant + nettoyage app.ts

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,16 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { Component } from '@angular/core';
 import { Navbar } from './components/navbar/navbar';
-
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule, MatSlideToggleModule, Navbar],
+  imports: [Navbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('frontend');
-}
+export class App {}
